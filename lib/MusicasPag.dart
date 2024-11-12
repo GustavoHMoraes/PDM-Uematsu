0e90b35b919e7ef3f0c0454481e639557cdedb0e
GustavoHMoraes <gustavo.rocillo@etec.sp.gov.br>
2024-11-12 13:43:24 -0300

    att

--- lib/MusicasPag.dart
+++ lib/MusicasPag.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter_app/main.dart';
 
-class CMusica{
+class CMusica {
   String audio;
   String titulo;
 
   CMusica(this.audio, this.titulo);
 }
 
-class MusicasPag extends StatelessWidget {
+class MusicasPag extends StatefulWidget {
   MusicasPag({Key? key}) : super(key: key);
 
-final CImg imgNU = CImg('assets/img/NobuoD.png', '', 1); 
+  @override
+  _MusicasPagState createState() => _MusicasPagState();
+}
+
+class _MusicasPagState extends State<MusicasPag> {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  bool _isPlaying = false;
+
+  final CImg imgNU = CImg('assets/img/NobuoD.png', '', 1); 
+  final CMusica audioOWA = CMusica('assets/audio/OneWingedAngel.mp3', 'One Winged Angel');
 
 
+  void _togglePlayPause() {
+    if (_isPlaying) {
+      _audioPlayer.pause();
+    } else {
+      _audioPlayer.play((audioOWA.audio)); 
+    }
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,20 +62,48 @@ final CImg imgNU = CImg('assets/img/NobuoD.png', '', 1);
       ),
       backgroundColor: const Color.fromARGB(255, 241, 243, 242),
       body: Center(
-        child: Container(
-          margin: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 160, 212, 220),
-            borderRadius: BorderRadius.circular(8),
-          ),
-          padding: const EdgeInsets.all(15),
-          child: const Text(
-            "Conteúdo da página de Músicas",
-            style: TextStyle(fontSize: 18, color: Colors.black),
-            textAlign: TextAlign.center,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,  
+              height: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(imgNU.img), 
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30), 
+            Text(
+              audioOWA.titulo,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20), 
+            ElevatedButton(
+              onPressed: _togglePlayPause,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 160, 212, 220),
+                padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Text(
+                _isPlaying ? 'Pausar' : 'Tocar',
+                style: const TextStyle(fontSize: 18, color: Colors.black),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
   }
 }
